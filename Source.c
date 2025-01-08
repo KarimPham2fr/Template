b123fd067967d83a874b5e70447e4a9e8cc31639
Phong.PT <phong.pt.hust@gmail.com>
2025-01-08 08:44:52 +0700

    Remove Header.h

--- Source.c
+++ Source.c
@@ -2,7 +2,7 @@
  * fileName.c
  *
  *  Created on: Jan 22th, 2023
- *      Author: Phong Pham-Thanh
+ *      Author: Phong Pham-Thanh.
  *       Email: Phong.PT.HUST@gmail.com
  *
 ==================================================================================================*/
